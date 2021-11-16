dcd202842acadb3bcf60635f7c0f702a758351dc
unknown <you@example.com>
2021-11-16 22:12:58 +0400

    Task

---
+++ assets/java.js
@@ -0,0 +1,54 @@
+//Calculator Task
+// function Calculator(x,y,op){
+//     switch(op){
+//         case '+':
+//            return x+y;
+//            break;
+//         case '-':
+//             return x-y;
+//             break;
+//         case '*':
+//             return x*y;
+//             break;
+//         case "/":
+//             return x/y;
+//             break;  
+//         }
+// }
+// console.log(Calculator(x,y,op))
+
+
+
+
+
+//Cut Eded Task
+//   function ArraySearch(){
+//       let arr = Array.from(arguments);
+//       const even= [1,2,3,4,5,6,7,8,9,10]
+//       for (let i = 0; i < even.length; i++) {
+//         if(i % 2!=0)
+//         {
+//          arr.push(even[i])
+//         }
+//       }
+//       console.log(arr)
+//   }
+//   ArraySearch();
+
+
+
+  
+//Arrayin icinde olub olmamasi
+//  function Search(num, arr) {
+//      for (let i = 0; i < arr.length; i++) {
+//        if (arr[i] == num) {
+//          return true;
+//        }
+//        if (arr[i] % 2 == 0) {
+//         console.log(arr[i]);
+//     }
+//      }
+//      return false;
+//    }
+//    let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//    console.log(Search(2, array));
